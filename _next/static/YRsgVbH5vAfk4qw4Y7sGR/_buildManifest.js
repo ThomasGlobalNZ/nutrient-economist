70d5438e72a03289868f3eaed4041b66699f5b82
ThomasGlobalNZ <thomas@globalsurvey.co.nz>
2026-02-13 22:06:00 +1300

    Updates

--- _next/static/YRsgVbH5vAfk4qw4Y7sGR/_buildManifest.js
+++ _next/static/YRsgVbH5vAfk4qw4Y7sGR/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nutrient-economist//_next/:path+",
+        "destination": "/nutrient-economist/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
